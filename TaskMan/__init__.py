6e0e951fcd36c8aaec4cc1c2b34285f16ac5a296
Kabir Sharma <kabirsharma2905@gmail.com>
2024-03-06 18:34:15 +0530

    Added call to init_db for TEST env

--- TaskMan/__init__.py
+++ TaskMan/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import get_config
-from .Database.database import database
+from .Database.database import database, init_db
 
 
 def create_app():
@@ -12,6 +12,11 @@ def create_app():
     # utils need to be imported before blueprints
     # routes to avoid a circular import
     from . import utils
+
+    # This calls SQLAlchemy's create_all() to build tables
+    # Will only be called in TEST environment
+    if app.config['INIT_DB']:
+        init_db()
     
     from .TaskAPI.routes import tasks
     from .AuthAPI.routes import auth
